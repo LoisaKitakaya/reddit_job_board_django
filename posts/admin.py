e34fe5eb25ce227f7c08f159fddb517e90953cc6
Loisa Kitakaya <kitakayaloisa@gmail.com>
2025-08-05 12:42:06 +0300

    created posts app models and migrations

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "post_title",
+        "post_trigger",
+        "post_time",
+    )
+    list_filter = ("subreddit", "post_category", "post_trigger")
+    search_fields = (
+        "post_title",
+        "reddit_post_id",
+        "subreddit",
+        "post_owner_reddit_username",
+    )
+    readonly_fields = ("id",)
+    fields = (
+        "id",
+        "reddit_post_id",
+        "post_title",
+        "post_url",
+        "post_owner_reddit_username",
+        "subreddit",
+        "post_category",
+        "post_trigger",
+        "post_time",
+    )
+    list_per_page = 25
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,62 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    TASK = "job"
+    OFFER = "offer"
+
+    TRIGGER = [
+        (TASK, "Job"),
+        (OFFER, "Offer"),
+    ]
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+    post_owner_reddit_username = models.CharField(
+        max_length=100,
+        unique=True,
+        db_index=True,
+    )
+    reddit_post_id = models.CharField(
+        max_length=20,
+        unique=True,
+    )
+    post_url = models.URLField(
+        max_length=200,
+    )
+    post_title = models.CharField(
+        max_length=300,
+    )
+    post_category = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    post_trigger = models.CharField(
+        max_length=20,
+        choices=TRIGGER,
+        blank=True,
+        null=True,
+    )
+    subreddit = models.CharField(
+        max_length=100,
+        db_index=True,
+    )
+    post_time = models.DateTimeField(
+        null=True,
+        blank=True,
+        db_index=True,
+    )
+
+    class Meta:
+        db_table = "posts"
+        ordering = ["-post_time"]
+        verbose_name = "Reddit Post"
+        verbose_name_plural = "Reddit Posts"
+
+    def __str__(self):
+        return f"{self.post_title}"
