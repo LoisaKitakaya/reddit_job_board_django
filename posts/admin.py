496a6076fe964200cbbc622d1de332516693a34e
Loisa Kitakaya <kitakayaloisa@gmail.com>
2025-08-18 12:49:09 +0300

    new static files and edited posts.admin file

--- posts/admin.py
+++ posts/admin.py
@@ -1,10 +1,5 @@
-from django.urls import path
 from posts.models import Post
-from django.urls import reverse
 from django.contrib import admin
-from django.contrib import messages
-from posts.tasks import generate_leads
-from django.http import HttpResponseRedirect
 
 
 @admin.register(Post)
@@ -40,40 +35,3 @@ class PostAdmin(admin.ModelAdmin):
     )
 
     list_per_page = 25
-
-    def has_add_permission(self, request):
-        return False
-
-    def get_urls(self):
-        urls = super().get_urls()
-
-        custom_urls = [
-            path(
-                "generate-leads/",
-                self.admin_site.admin_view(self.generate_leads_view),
-                name="post-generate-leads",
-            ),
-        ]
-
-        return custom_urls + urls
-
-    def generate_leads_view(self, request):
-        try:
-            generate_leads(posts_limit=20)  # type: ignore
-
-            self.message_user(
-                request,
-                "Lead generation task successful",
-                level=messages.SUCCESS,
-            )
-
-        except Exception as e:
-            self.message_user(
-                request,
-                f"Error queuing lead generation task: {str(e)}",
-                level=messages.ERROR,
-            )
-
-        return HttpResponseRedirect(reverse("admin:posts_post_changelist"))
-
-    change_list_template = "admin/posts/post/change_list.html"
